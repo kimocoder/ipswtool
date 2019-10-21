8fbdf963d1a7ac21530b958a5c0f18af182d7975
wxb1ank <wxblank@gmail.com>
2019-10-21 02:33:32 -0400

    added artie and initial main()

---
+++ src/ipswtool.cpp
@@ -0,0 +1,41 @@
+// wxblank
+// 10/20/19
+
+#include <iostream>
+#include <vector>
+
+#include "include/artie.h"
+
+#define PROGRAM_NAME "ipswtool"
+#define PROGRAM_DESCRIPTION "Read IPSW file properties"
+#define PROGRAM_AUTHOR "wxblank"
+#define PROGRAM_WEBSITE "https://wxb1ank.github.io/"
+
+#define VERSION "1.0"
+
+std::vector<arg_t> args = {
+	{'i', "ipsw"},
+	{'h', "help"},
+	{NULL, "version"}
+};
+
+int main(int argc, char *argv[]) {
+	artie art(PROGRAM_NAME, PROGRAM_DESCRIPTION, PROGRAM_AUTHOR, argv);
+	art.website = PROGRAM_WEBSITE;
+	art.version = VERSION;
+
+	#ifdef BUILD_NUMBER
+	art.build = BUILD_NUMBER;
+	#endif
+
+	#ifdef BUILD_DATE
+	art.builddate = BUILD_DATE;
+	#endif
+
+	art.args = args;
+
+	if(argc == 1) { art.print_help(); exit(0); }
+	art.print_version();
+	
+	return 0;
+}
